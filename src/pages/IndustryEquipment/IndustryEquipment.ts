f1bd9eb26ffc2502061a07ed4ab0b5e00d4d0ccf
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-22 16:37:17 +0530

    industry equipment change

--- src/pages/IndustryEquipment/IndustryEquipment.ts
+++ src/pages/IndustryEquipment/IndustryEquipment.ts
@@ -8,11 +8,40 @@ import { ServiceSingletonProvider } from '../../providers/service-singleton/serv
 })
 export class IndustryEquipmentPage {
   public title:any="Industry Equipment";
+  public IndustryInfo:any;
+  public industyDeatils:any='Industry Equipments';
+  public IndustryID:any;
   constructor(public navCtrl: NavController,
               public navParms: NavParams,
               public webService: ServiceSingletonProvider) {
-                  
+    if(this.navParms.get('IndustryInfo')){
+      this.IndustryInfo=this.navParms.get('IndustryInfo');
+    }
+    console.log(this.IndustryInfo);
+    this.title=this.IndustryInfo.indu_name;
+    this.IndustryID=this.IndustryInfo._id;
+    this.CallWebServices();
+  }
+
+  addInduEquipment(){
 
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.CallWebServices();
+      refresher.complete();
+    }, 2000);
+  }
+
+  CallWebServices(){
+    this.webService.presentLoading();
+    this.webService.getIndustryAllEquip(this.IndustryID).then(data=>{
+      this.webService.stopLoading();
+      console.log(data);
+    })
+  }
 }
