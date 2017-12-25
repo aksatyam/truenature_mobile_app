3bd3b0cec2e00172b963b3d40e2ed26eecd1bab1
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-25 21:15:26 +0530

    industry shift listing page

--- src/pages/IndustryShift/IndustryShift.ts
+++ src/pages/IndustryShift/IndustryShift.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
+import { ServiceSingletonProvider } from '../../providers/service-singleton/service-singleton';
 
 @Component({
   selector: 'page-IndustryShift',
@@ -7,9 +8,54 @@ import { NavController } from 'ionic-angular';
 })
 export class IndustryShiftPage {
   public title:any="Industry Shift";
+  public IndustryInfo:any;
+  public industyDeatils:any='Industry Shifts';
+  public IndustryID:any;
+  public Status:boolean=false;
+  public ShiftData:any;
   
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public navParms: NavParams,
+    public webService: ServiceSingletonProvider) {
+      if(this.navParms.get('IndustryInfo')){
+        this.IndustryInfo=this.navParms.get('IndustryInfo');
+      }
+      console.log(this.IndustryInfo);
+      this.title=this.IndustryInfo.indu_name;
+      this.IndustryID=this.IndustryInfo._id;
+      this.CallWebServices();
+  }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.CallWebServices();
+      refresher.complete();
+    }, 2000);
+  }
+
+  ionViewDidEnter(){
+    if(this.Status){
+      this.CallWebServices();
+    }
+    this.Status=true;
+  }
+  
+  addInduShift(){
+
+  }
+
+  CallWebServices(){
+    this.webService.presentLoading();
+    this.webService.getIndustryAllShift(this.IndustryID).then(data=>{
+      this.webService.stopLoading();
+      console.log(data);
+      if(data['data']){
+        this.ShiftData=data['data'];
+      }
+    })
   }
 
 }
