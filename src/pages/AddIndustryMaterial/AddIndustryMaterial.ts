63bb94141c196352c513647a0f786c3fb97b9ba5
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-25 14:29:09 +0530

    add industry material page completed

--- src/pages/AddIndustryMaterial/AddIndustryMaterial.ts
+++ src/pages/AddIndustryMaterial/AddIndustryMaterial.ts
@@ -11,6 +11,8 @@ export class AddIndustryMaterialPage {
   public title:any="Add Materials";
   public MaterialName:any='';
   public Description:any='';
+  public inspectionPara='';
+  public qualityCheckPara='';
   constructor(public navCtrl: NavController,
         public navParms: NavParams,
         public webService: ServiceSingletonProvider) {
@@ -19,11 +21,44 @@ export class AddIndustryMaterialPage {
         }
   }
 
-  SaveEquip(){
-
+  SaveMaterial(){
+    if(this.MaterialName=='' && this.Description=='' && this.inspectionPara=='' && this.qualityCheckPara=='' ){
+      this.webService.presentAlert('Alert!','Fill all the blank fields.');
+    }
+    else if(this.MaterialName==''){
+      this.webService.presentAlert('Alert!','Enter the material name');
+    }
+    else if(this.Description==''){
+      this.webService.presentAlert('Alert!','Enter the material description');
+    }
+    else if(this.inspectionPara==''){
+      this.webService.presentAlert('Alert!','Enter the material inspection paremater');
+    }
+    else if(this.qualityCheckPara==''){
+      this.webService.presentAlert('Alert!','Enter the material qualitycheck paremeter');
+    }
+    else{
+      this.postData();
+    }
   }
 
   Reset(){
+    this.MaterialName='';
+    this.Description='';
+    this.inspectionPara='';
+    this.qualityCheckPara='';
+  }
 
+  postData(){
+    var myKeyVals={indu_id:this.IndustryId, name:this.MaterialName, description:this.Description, inspection_parem:this.inspectionPara, qulaitycheck_parem:this.qualityCheckPara};
+    this.webService.presentLoading();
+    this.webService.postIndustryOneMaterial(myKeyVals).then(data=>{
+      this.webService.stopLoading();
+      console.log(data);
+      if(data['msg']){
+        this.Reset();
+        this.webService.presentAlert('Success!','Material Added Successfully.');
+      }
+    })
   }
 }
