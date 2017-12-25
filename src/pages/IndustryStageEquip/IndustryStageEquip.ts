f62f5e70763df6d84a9fed10562380564b53daed
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-25 15:44:31 +0530

    industry Stage list page done

--- src/pages/IndustryStageEquip/IndustryStageEquip.ts
+++ src/pages/IndustryStageEquip/IndustryStageEquip.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController,NavParams } from 'ionic-angular';
+import { ServiceSingletonProvider } from '../../providers/service-singleton/service-singleton';
 @Component({
   selector: 'page-IndustryStageEquip',
   templateUrl: 'IndustryStageEquip.html'
 })
 export class IndustryStageEquipPage {
   public title:any="Stage Equipment";
-  constructor(public navCtrl: NavController) {
+  public IndustryInfo:any;
+  public industyDeatils:any='Industry Stage Equipment';
+  public IndustryID:any;
+  public Status:boolean=false;
+  public StageEquipmentData:any;
+  constructor(public navCtrl: NavController,
+              public navParms: NavParams,
+              public webService: ServiceSingletonProvider) {
+                if(this.navParms.get('IndustryInfo')){
+                  this.IndustryInfo=this.navParms.get('IndustryInfo');
+                }
+                console.log(this.IndustryInfo);
+                this.title=this.IndustryInfo.indu_name;
+                this.IndustryID=this.IndustryInfo._id;
+                this.CallWebServices();
+  }
 
+  addInduStageEquip(){
+
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.CallWebServices();
+      refresher.complete();
+    }, 2000);
+  }
+
+  ionViewDidEnter(){
+    if(this.Status){
+      this.CallWebServices();
+    }
+    this.Status=true;
+  }
+
+  CallWebServices(){
+    this.webService.presentLoading();
+    this.webService.getIndustryAllSatgeEquip(this.IndustryID).then(data=>{
+      this.webService.stopLoading();
+      console.log(data);
+      if(data['data']){
+        this.StageEquipmentData=data['data'];
+      }
+    })
   }
 
 }
