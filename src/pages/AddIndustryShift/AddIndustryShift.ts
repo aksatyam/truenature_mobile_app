68a5b538be2af4b56c25448490d1f2e96d31ea50
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-25 23:59:31 +0530

    add industry shift

--- src/pages/AddIndustryShift/AddIndustryShift.ts
+++ src/pages/AddIndustryShift/AddIndustryShift.ts
@@ -6,9 +6,15 @@ import { ServiceSingletonProvider } from '../../providers/service-singleton/serv
   selector: 'page-AddIndustryShift',
   templateUrl: 'AddIndustryShift.html'
 })
+
 export class AddIndustryShiftPage {
   public IndustryId:any;
   public title:any="Add Shift";
+  public ShiftName:any='';
+  public Description:any='';
+  public ShiftTime:any='';
+  public timeStarts:any;
+  public timeEnds:any;
   constructor(public navCtrl: NavController,
         public navParms: NavParams,
         public webService: ServiceSingletonProvider) {
@@ -17,8 +23,16 @@ export class AddIndustryShiftPage {
         }
   }
 
-  SaveShift(){
+  timeChange(){
+    console.log(this.timeStarts);
+    console.log(this.timeEnds);
+  }
 
+
+  SaveShift(){
+    if(this.ShiftName=='' && this.ShiftTime=='' && this.timeStarts=='' && this.timeEnds=='' && this.Description){
+      this.webService.presentAlert('Success!','')
+    }
   }
 
   Reset(){
