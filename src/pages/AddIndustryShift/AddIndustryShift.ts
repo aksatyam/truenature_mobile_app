eaa346e7c678bd6c419ceeb3ae7810e173f86b6f
aksatyam <ashishsatyam.kmr@gmail.com>
2017-12-26 08:16:33 +0530

    Shift page code completed

--- src/pages/AddIndustryShift/AddIndustryShift.ts
+++ src/pages/AddIndustryShift/AddIndustryShift.ts
@@ -13,8 +13,8 @@ export class AddIndustryShiftPage {
   public ShiftName:any='';
   public Description:any='';
   public ShiftTime:any='';
-  public timeStarts:any;
-  public timeEnds:any;
+  public timeStarts:any=new Date().toISOString();
+  public timeEnds:any=new Date().toISOString();
   constructor(public navCtrl: NavController,
         public navParms: NavParams,
         public webService: ServiceSingletonProvider) {
@@ -26,16 +26,67 @@ export class AddIndustryShiftPage {
   timeChange(){
     console.log(this.timeStarts);
     console.log(this.timeEnds);
+    if(this.timeStarts==this.timeEnds){
+      this.webService.presentAlert('Alert!','Both the times should not be same');
+    }
+    else if(this.timeStarts>this.timeEnds){
+      this.webService.presentAlert('Alert!','Start Time should not be greater than End Time');
+    }
   }
 
 
   SaveShift(){
     if(this.ShiftName=='' && this.ShiftTime=='' && this.timeStarts=='' && this.timeEnds=='' && this.Description){
-      this.webService.presentAlert('Success!','')
+      this.webService.presentAlert('Success!','Fill the blank fields');
+    }
+    else if(this.ShiftName==''){
+      this.webService.presentAlert('Alert!','Enter shift name');
+    }
+    else if(this.ShiftTime==''){
+      this.webService.presentAlert('Alert!','Enter shift time');
+    }
+    else if(this.timeStarts==''){
+      this.webService.presentAlert('Alert!','Select start time');
+    }
+    else if(this.timeEnds==''){
+      this.webService.presentAlert('Alert!','Select end time');
+    }
+    else if(this.timeStarts==this.timeEnds){
+      this.webService.presentAlert('Alert!','Both the times should not be same');
+    }
+    else if(this.timeStarts>this.timeEnds){
+      this.webService.presentAlert('Alert!','Start Time should not be greater than End Time');
+    }
+    else if(this.Description==''){
+      this.webService.presentAlert('Alert!','Enter shift description');
+    }
+    else{
+      this.postData();
     }
   }
 
   Reset(){
-      
+      this.ShiftName='';
+      this.ShiftTime='';
+      this.Description='';
+  }
+
+  postData(){
+    var myKeyVals={ indu_id:this.IndustryId, 
+                    shift_name:this.ShiftName, 
+                    shift_time:this.ShiftTime, 
+                    start_time:this.timeStarts, 
+                    end_time:this.timeEnds,
+                    shift_desc:this.Description};
+
+    this.webService.presentLoading();
+    this.webService.postIndustryOneShift(myKeyVals).then(data=>{
+      this.webService.stopLoading();
+      console.log(data);
+      if(data['msg']){
+        this.Reset();
+        this.webService.presentAlert('Success', 'Shift Saved Successfully');
+      }
+    })
   }
 }
